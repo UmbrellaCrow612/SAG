c743ae703bd7f7edacd8956f51ebf850b5f41f87
Yousaf Wazir <umbrellaman612@gmail.com>
2025-06-18 16:01:50 +0100

    openGraph added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,7 @@ import CallToActionSection from "@/components/CallToActionSection";
 import LnadingHeroSection from "@/components/LandingHero";
 import UpcomingEventsSection from "@/components/UpcomingEventsSection";
 import WhatWeDoSection from "@/components/WhatWeDoSection";
+import { sagMetaData } from "@/metaData";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -16,6 +17,23 @@ export const metadata: Metadata = {
     "Anime watch parties",
     "Sheffield Discord anime",
   ],
+  openGraph: {
+    title: "Sheffield Anime Group – Join Sheffield's Vibrant Anime Community",
+    description:
+      "Join Sheffield Anime Group to watch, discuss, and celebrate anime with fellow fans. Enjoy watch parties, cosplay events, quizzes, and more in the Steel City!",
+    type: "website",
+    locale: "en_GB",
+    url: sagMetaData.siteUrl,
+    siteName: "Sheffield Anime Group",
+    images: [
+      {
+        url: "/og_img.png",
+        width: 1200,
+        height: 630,
+        alt: "Sheffield Anime Group - Join Sheffield's anime community for watch parties, cosplay events, and more",
+      },
+    ],
+  },
 };
 
 export default function Home() {
