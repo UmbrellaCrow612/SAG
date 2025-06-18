7a561793ebfdbe4c9744d8a13925c972e022eff9
Yousaf Wazir <umbrellaman612@gmail.com>
2025-06-18 15:45:36 +0100

    Update robots.ts

--- frontend/src/app/robots.ts
+++ frontend/src/app/robots.ts
@@ -3,10 +3,16 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "Bingbot",
+        allow: "/",
+      },
+    ],
     sitemap: `${sagMetaData.siteUrl}/sitemap.xml`,
   };
 }
